29642f3e5017b4ed3d0f9c4326cb74da79ec19e6
Mehmet Kocadag <59103139+marksmandantus@users.noreply.github.com>
2021-03-06 06:21:53 -0500

    Add files via upload

---
+++ mail_spammer.py
@@ -0,0 +1,28 @@
+from smtplib import SMTP
+import time
+
+while True:
+
+    try:
+        subject = "Test"
+        message = "This is a test message."
+        content = "Subject:{0}\n\n{1}.".format(subject,message)
+
+        my_mail_address = "Enter your mail address"
+        my_password = "Enter your password"
+
+        send_to = "Enter the e-mail to be sent "
+
+        mail = SMTP("smtp-mail.outlook.com",587)
+        mail.ehlo()
+        mail.starttls()
+        mail.login(my_mail_address,my_password)
+        mail.sendmail(my_mail_address,send_to,content.encode("utf-8"))
+        print("Sending e-mail successful!")
+
+        time.sleep(1)
+
+    except Exception as e:
+        print("Something went wrong!\n{0}".format(e))    
+
+
